627f05a3b731935a92d776408491d29476895303
Illya Reutskyy <iliyareutsky@gmail.com>
2021-01-30 13:04:00 -0500

    Creates the project

---
+++ src/main/java/ca/concordia/risk/Game.java
@@ -0,0 +1,12 @@
+package ca.concordia.risk;
+
+/**
+ * Main Game class containing the Game Loop
+ */
+public class Game 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Starting the game..." );
+    }
+}
---
+++ src/test/java/ca/concordia/risk/GameTest.java
@@ -0,0 +1,20 @@
+package ca.concordia.risk;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for the main Game class.
+ */
+public class GameTest 
+{
+    /**
+     * Sample test 
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
