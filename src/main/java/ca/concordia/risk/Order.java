5df815f74fc149dcb2b34c23e0da32ae4e39b46a
enrique <menriquemcm@gmail.com>
2021-02-11 23:00:02 -0600

    Creation of Order interface

---
+++ src/main/java/ca/concordia/risk/Order.java
@@ -0,0 +1,14 @@
+package ca.concordia.risk;
+
+/**
+ * Interface class for the Orders entities.
+ * @author Enrique
+ *
+ */
+public interface Order {
+	
+	/**
+	 * Order API for executing an order.
+	 */
+	public void Exec();
+}
