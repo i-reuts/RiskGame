9842993cc2ef0b5aa17db9a9ba4f1d1de72181d5
Illya Reutskyy <iliyareutsky@gmail.com>
2021-02-21 15:15:19 -0500

    Add loadmap command implementation

--- src/main/java/ca/concordia/risk/io/commands/LoadMapCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/LoadMapCommand.java
@@ -1,7 +1,11 @@
 package ca.concordia.risk.io.commands;
 
+import java.io.FileNotFoundException;
+
 import ca.concordia.risk.game.GameEngine;
+import ca.concordia.risk.game.GameMap;
 import ca.concordia.risk.io.views.ConsoleView;
+import ca.concordia.risk.services.MapLoader;
 
 /** Command representing <i>"loadmap"</i> operation. */
 public class LoadMapCommand implements Command {
@@ -20,11 +24,20 @@ public class LoadMapCommand implements Command {
 	/** Loads the requested map file in play mode. */
 	@Override
 	public void execute() {
-		// TODO Replace with actual implementation
 		ConsoleView l_view = GameEngine.GetView();
-		l_view.display("\nExecuting loadmap command with filename: " + d_filename + "\n");
+		GameMap l_gameMap = null;
+		try {
+			l_gameMap = MapLoader.LoadMap(d_filename);
+			l_view.display("Map succesfully loaded from file");
+			GameEngine.SetMap(l_gameMap);
+			GameEngine.SwitchToStartupMode();
+		} catch (FileNotFoundException e) {
+			l_view.display("Map file with filename " + d_filename + " does not exist");
+		} catch (MapLoader.FileParsingException e) {
+			l_view.display("Error while loading the map file");
+			l_view.display(e.getMessage());
+		}
 
-		GameEngine.SwitchToStartupMode();
 	}
 
 }
