b109e7d6aef421d7c5793bea66e0c3c0f256c998
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2021-02-22 20:25:39 +0530

    add fetching Country details in SaveMapCommand class

--- src/main/java/ca/concordia/risk/io/commands/SaveMapCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/SaveMapCommand.java
@@ -25,6 +25,7 @@ public class SaveMapCommand implements Command {
 	}
 
 	/** Saves the active Map into the requested map file. */
+	@SuppressWarnings("unused")
 	@Override
 	public void execute() {
 		ConsoleView l_view = GameEngine.GetView();
@@ -38,34 +39,39 @@ public class SaveMapCommand implements Command {
 			Map<Continent, Integer> l_active_continents;
 			Map<Country, Integer> l_active_countries;
 		
-			l_active_continents = fetchAllContinents(l_gameMap, l_names_continents);	
-			l_active_countries = fetchAllCountries(l_gameMap, l_names_countries);
+			l_active_continents = fetchActiveContinents(l_gameMap, l_names_continents);	
+			l_active_countries = fetchActiveCountries(l_gameMap, l_names_countries);
 		}
 		else {
 			l_view.display("No map to be saved in the file named '" + d_filename + ".map'"); 
 		}
 	}
 
-	private Map<Country, Integer> fetchAllCountries(GameMap l_gameMap, String[] l_names_countries) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Map<Continent, Integer> fetchAllContinents(GameMap p_gameMap, String[] p_names_continents) {
+	private Map<Continent, Integer> fetchActiveContinents(GameMap p_gameMap, String[] p_names_continents) {
 		
 		Arrays.sort(p_names_continents); //sorts all the active Continents by ascending order (alphabetically) 
-		Map<Continent, Integer> l_continent_map = new HashMap<>(); //Empty Map
+		Map<Continent, Integer> l_active_continents = new HashMap<>(); //Empty Map
 		int[] l_id_continents = IntStream.range(1, p_names_continents.length+1).toArray(); //generates continent IDs for all continent names 	
 		Continent l_continent;
 		
 		//this loops generates a HashMap which consist of <Continent, Integer> in alphabetical order.
 		for(int i = 0; i < p_names_continents.length; i++) {
 			l_continent = p_gameMap.getContinent(p_names_continents[i]);
-			l_continent_map.put(l_continent, l_id_continents[i]);
+			l_active_continents.put(l_continent, l_id_continents[i]);
 		} 
-		
-		return l_continent_map;
-		
+		return l_active_continents;
 	}
-
+	
+	private Map<Country, Integer> fetchActiveCountries(GameMap p_gameMap, String[] p_names_countries) {
+		Arrays.sort(p_names_countries);
+		Map<Country, Integer> l_active_countries = new HashMap<>();
+		int[] l_id_countries = IntStream.range(1, p_names_countries.length+1).toArray();
+		Country l_country;
+		
+		for(int i = 0; i < p_names_countries.length; i++) {
+			l_country = p_gameMap.getCountry(p_names_countries[i]);
+			l_active_countries.put(l_country, l_id_countries[i]);
+		}
+		return l_active_countries;
+	}
 }
