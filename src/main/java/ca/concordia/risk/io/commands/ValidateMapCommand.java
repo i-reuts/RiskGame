ee969d8d59a86a0a1b555623482127d7c871c050
Ishika Dhall <ishikadhall11@gmail.com>
2021-02-23 19:39:18 +0530

    Added a class Graph in ValidateMapCommand.java

--- src/main/java/ca/concordia/risk/io/commands/ValidateMapCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/ValidateMapCommand.java
@@ -1,8 +1,35 @@
 package ca.concordia.risk.io.commands;
 
+import java.util.ArrayList;
+import java.util.List;
 import ca.concordia.risk.game.GameEngine;
 import ca.concordia.risk.game.GameMap;
 import ca.concordia.risk.io.views.ConsoleView;
+
+/**
+ * @author ishika
+ *
+ */
+class Graph {
+	private List<List<Integer>> graph;
+	private boolean visited[];
+	private int nodes;
+
+	/**
+	 * Constructor of class Graph
+	 * 
+	 * @param nodes
+	 */
+	public Graph(int nodes) {
+		graph = new ArrayList<>();
+		visited = new boolean[nodes];
+		this.nodes = nodes;
+
+		for (int i = 0; i < nodes; i++) {
+			graph.add(i, new ArrayList<>());
+		}
+	}
+}
 
 /** Command representing <i>"validatemap"</i> operation. */
 public class ValidateMapCommand implements Command {
@@ -13,14 +40,11 @@ public class ValidateMapCommand implements Command {
 		// TODO Replace with actual implementation
 		ConsoleView l_view = GameEngine.GetView();
 		l_view.display("\nValidating the active map\n");
-		
-		GameMap l_gameMap = GameEngine.GetMap();
-		if (l_gameMap != null) {
-			//l_gameMap.isConnected();
-			
-		} else {
-			l_view.display("Please load a map to be validated first");
-		}
-	}
 
+		GameMap l_gameMap = GameEngine.GetMap();
+		int nodes = l_gameMap.numberOfCountries();
+
+		Graph a = new Graph(nodes);
+
+	}
 }
