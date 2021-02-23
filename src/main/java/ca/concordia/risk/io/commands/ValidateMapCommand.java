b94683eb2039cb54a0672df434fd4ddca38af4a6
Ishika Dhall <ishikadhall11@gmail.com>
2021-02-23 20:08:37 +0530

    Updated the dfs method and added proper documentation comments in ValidateMapCommand.java

--- src/main/java/ca/concordia/risk/io/commands/ValidateMapCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/ValidateMapCommand.java
@@ -3,6 +3,8 @@ package ca.concordia.risk.io.commands;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
+
 import ca.concordia.risk.game.GameEngine;
 import ca.concordia.risk.game.GameMap;
 import ca.concordia.risk.io.views.ConsoleView;
@@ -12,56 +14,82 @@ import ca.concordia.risk.io.views.ConsoleView;
  *
  */
 class Graph {
-	private List<List<Integer>> graph;
-	private boolean visited[];
-	private int nodes;
+	private List<List<Integer>> d_graph;
+	private boolean d_visited[];
+	private int d_nodes;
 
 	/**
 	 * Constructor of class Graph
 	 * 
 	 * @param nodes
 	 */
-	public Graph(int nodes) {
-		graph = new ArrayList<>();
-		visited = new boolean[nodes];
-		this.nodes = nodes;
+	public Graph(int p_nodes) {
+		d_graph = new ArrayList<>();
+		d_visited = new boolean[p_nodes];
+		this.d_nodes = p_nodes;
 
-		for (int i = 0; i < nodes; i++) {
-			graph.add(i, new ArrayList<>());
+		for (int i = 0; i < p_nodes; i++) {
+			d_graph.add(i, new ArrayList<>());
 		}
 	}
 	
 	/**
-	 * @param a
-	 * @param b
+	 * Adds edges to the adjacency list
+	 * 
+	 * @param a Vertex Country
+	 * @param b Neighboring country
 	 */
 	public void addEdge(int a, int b) {
-		graph.get(a).add(b);
+		d_graph.get(a).add(b);
 	}
 
 
 	/**
-	 * @return
+	 * Checks whether the given directed graph is fully connected or not.
+	 * 
+	 * @return<code>true</code> if the given directed graph is fully connected.<br>
+	 * 		  <code>false</code> if the given directed graph not is fully connected.
 	 */
 	public boolean isConnected() {
 
-		for (int i = 0; i < nodes; i++) {
+		for (int i = 0; i < d_nodes; i++) {
 			dfs(i);
 
-			for (int j = 0; j < nodes; j++) {
-				if (!visited[j]) {
+			for (int j = 0; j < d_nodes; j++) {
+				if (!d_visited[j]) {
 					return false;
 				}
 			}
-
-			Arrays.fill(visited, false);
+			Arrays.fill(d_visited, false);
 		}
-
 		return true;
 	}
 
+	
+	/**
+	 * Uses depth first search for checking whether the given directed 
+	 * graph is fully connected or not
+	 *  
+	 * @param start
+	 */
+	public void dfs(int p_start) {
+		Stack<Integer> stack = new Stack<>();
 
-	public void dfs(int start) {
+		stack.push(p_start);
+		d_visited[p_start] = true;
+
+		while (!stack.isEmpty()) {
+			Integer node = stack.pop();
+
+			List<Integer> neighboursList = d_graph.get(node);
+
+			for (Integer neighbour : neighboursList) {
+				if (!d_visited[neighbour]) {
+					stack.push(neighbour);
+					d_visited[neighbour] = true;
+				}
+			}
+		}
 	}
 }
 
