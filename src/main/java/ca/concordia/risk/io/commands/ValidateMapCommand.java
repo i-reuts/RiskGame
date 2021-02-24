49c89b9e9fe33c525b96065cc7450af082080515
Ishika Dhall <ishikadhall11@gmail.com>
2021-02-24 16:56:26 +0530

    Validating map as whole

--- src/main/java/ca/concordia/risk/io/commands/ValidateMapCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/ValidateMapCommand.java
@@ -2,149 +2,81 @@ package ca.concordia.risk.io.commands;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.Stack;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 import ca.concordia.risk.GameEngine;
+import ca.concordia.risk.game.Continent;
 import ca.concordia.risk.game.Country;
-import ca.concordia.risk.game.GameMap;
 import ca.concordia.risk.io.views.ConsoleView;
 
 /**
- * @author ishika
- *
+ * Command representing <i>"validatemap"</i> operation.
+ * @author ishika 
+ * 
  */
-class Graph {
-	private List<List<Integer>> d_graph;
-	private boolean d_visited[];
-	private int d_nodes;
-
-	/**
-	 * Constructor of class Graph
-	 * 
-	 * @param p_nodes Total number of Countries/Vertices in the given map
-	 */
-	public Graph(int p_nodes) {
-		d_graph = new ArrayList<>();
-		d_visited = new boolean[p_nodes];
-		this.d_nodes = p_nodes;
-
-		for (int i = 0; i < p_nodes; i++) {
-			d_graph.add(i, new ArrayList<>());
-		}
-	}
-
-	/**
-	 * Adds edges to the adjacency list
-	 * 
-	 * @param a Vertex Country
-	 * @param b Neighboring country
-	 */
-	public void addEdge(int a, int b) {
-		d_graph.get(a).add(b);
-	}
-
-	/**
-	 * Checks whether the given directed graph is fully connected or not.
-	 * 
-	 * @return<code>true</code> if the given directed graph is fully connected.<br>
-	 * <code>false</code> if the given directed graph not is fully connected.
-	 */
-	public boolean isConnected() {
-
-		for (int i = 0; i < d_nodes; i++) {
-			dfs(i);
-
-			for (int j = 0; j < d_nodes; j++) {
-				if (!d_visited[j]) {
-					return false;
-				}
-			}
-			Arrays.fill(d_visited, false);
-		}
-		return true;
-	}
-
-	/**
-	 * Uses depth first search for checking whether the given directed graph is
-	 * fully connected or not
-	 * 
-	 * @param p_start
-	 */
-	public void dfs(int p_start) {
-		Stack<Integer> stack = new Stack<>();
-
-		stack.push(p_start);
-		d_visited[p_start] = true;
-
-		while (!stack.isEmpty()) {
-			Integer node = stack.pop();
-
-			List<Integer> neighboursList = d_graph.get(node);
-
-			for (Integer neighbour : neighboursList) {
-				if (!d_visited[neighbour]) {
-					stack.push(neighbour);
-					d_visited[neighbour] = true;
-				}
-			}
-		}
-	}
-}
-
-/** Command representing <i>"validatemap"</i> operation. */
 public class ValidateMapCommand implements Command {
 
 	/** Validates the active Map. */
 	@Override
 	public void execute() {
+
 		// TODO Replace with actual implementation
-		/* ConsoleView l_view = GameEngine.GetView();
+		ConsoleView l_view = GameEngine.GetView();
 		l_view.display("\nValidating the active map\n");
 
-		try {
-			GameMap l_gameMap = GameEngine.GetMap();
-			String[] d_nameCountries = l_gameMap.getArrayOfCountries();
-			int[] d_countryId = IntStream.range(1, d_nameCountries.length + 1).toArray();
-			Country c;
-			Map<Country, Integer> d_m = new HashMap<Country, Integer>();
-			Set<Country> d_neighborCountry;
-			int d_nodes = l_gameMap.numberOfCountries();
-
-			Graph obj = new Graph(d_nodes);
-
-			for (int i = 0; i < l_gameMap.numberOfCountries(); i++) {
-				c = l_gameMap.getCountry(d_nameCountries[i]);
-				d_m.put(c, d_countryId[i]);
-			}
-
-			d_m = d_m.entrySet().stream().sorted(Entry.comparingByValue())
-					.collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-
-			for (Entry<Country, Integer> e : d_m.entrySet()) {
-				c = e.getKey();
-				int d_homeId = d_m.get(c);
-				// System.out.println(c.getName() + " : " + d_homeId + "\n");
-				d_neighborCountry = c.getNeighbors();
-				for (Country c1 : d_neighborCountry) {
-					int d_neighborId = d_m.get(c1);
-					// System.out.println(d_neighborId);
-					obj.addEdge(d_homeId - 1, d_neighborId - 1);
-				}
-				System.out.println();
-			}
-
-			System.out.println(obj.isConnected());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}*/
+		boolean d_validate = validate();
+		if (d_validate == true) {
+			System.out.println("The given map is a valid map");
+		} else {
+			System.out.println("Not a valid map");
+		}
 	}
+
+	private boolean validate() {
+		Set<Country> l_countries = new HashSet<Country>(GameEngine.GetMap().getCountries());
+		if (l_countries.isEmpty()) {
+			return false;
+		}
+		// Validate the graph as a whole
+		for (Country l_country : l_countries) {
+			Set<Country> l_visited = new HashSet<Country>();
+			dfs(l_country, l_visited, l_countries);
+			if (l_visited.size() != l_countries.size()) {
+				return false;
+			}
+		}
+		// Validate each continent
+		List<Continent> l_continents = GameEngine.GetMap().getContinents();
+		for (Continent l_continent : l_continents) {
+			Set<Country> l_continentCountries = l_continent.getCountries();
+			if (l_continentCountries.isEmpty()) {
+				return false;
+			}
+			Set<Country> l_visited = new HashSet<Country>();
+			dfs(l_continentCountries.iterator().next(), l_visited, l_continentCountries);
+			if (l_visited.size() != l_continentCountries.size()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void dfs(Country p_startNode, Set<Country> p_visited, Set<Country> p_included) {
+		Stack<Country> l_stack = new Stack<Country>();
+		l_stack.push(p_startNode);
+		p_visited.add(p_startNode);
+		while (!l_stack.isEmpty()) {
+			Country l_node = l_stack.pop();
+			Set<Country> l_neighbourSet = l_node.getNeighbors();
+			for (Country l_neighbour : l_neighbourSet) {
+				if (!p_visited.contains(l_neighbour) && p_included.contains(l_neighbour)) {
+					l_stack.push(l_neighbour);
+					p_visited.add(l_neighbour);
+				}
+			}
+		}
+	}
+
 }
