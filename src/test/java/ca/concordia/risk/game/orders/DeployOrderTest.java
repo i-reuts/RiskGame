3da3a5f54d8a8dfa27e1afad301477b1436783a6
Ishika Dhall <ishikadhall11@gmail.com>
2021-03-19 01:28:23 +0530

    Test case for checking a succesful deployment of order in DeployOrder class for checking refractoring

--- src/test/java/ca/concordia/risk/game/orders/DeployOrderTest.java
+++ src/test/java/ca/concordia/risk/game/orders/DeployOrderTest.java
@@ -44,4 +44,31 @@ public class DeployOrderTest {
 				//assertEquals(d_deployorder.getStatus(),"Deployment failed: " + l_country1 + " no longer owned by Player B");
 				assertTrue(d_deployorder.getStatus().startsWith("Deployment failed: "));				
 	}
+	
+	@Test
+	public void deploymentPasstest() {
+		DeployOrder d_deployorder;
+		// Create sample map with a continent and countries
+				GameMap l_map = new GameMap();
+				Continent l_continent = new Continent("Test Continent", 10);
+				l_map.addContinent(l_continent);
+				for (int l_i = 0; l_i < 5; l_i++) {
+					l_map.addCountry(new Country("Country " + l_i, l_continent));
+				}
+				List<Country> l_countries = l_map.getCountries();
+				Country l_country1 = l_countries.get(0);
+				//Country l_country2 = l_countries.get(1);
+
+				// Create a player
+				Player l_player = new Player("Player A");
+				Player l_player2 = new Player("Player B");
+
+				// Add 1st country to countries owned by the player
+				l_player.addCountry(l_country1);
+				d_deployorder = new DeployOrder(l_player, l_country1, 4);
+				d_deployorder.execute();
+				
+				assertEquals(d_deployorder.getStatus(),"Player A deployed 4 armies to Country 0");
+				//assertTrue(d_deployorder.getStatus().startsWith("Deployment failed: "));				
+	}
 }
