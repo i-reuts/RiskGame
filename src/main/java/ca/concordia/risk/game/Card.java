5fa1c9b00efbac1d1b765bebfe9ca50036ad1b1f
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2021-03-19 00:02:07 +0530

    update issueRandomCard and default constructor

--- src/main/java/ca/concordia/risk/game/Card.java
+++ src/main/java/ca/concordia/risk/game/Card.java
@@ -1,16 +1,23 @@
 package ca.concordia.risk.game;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Card 
 {
 	private enum d_cardType
 	{
-		BOMB, BLOCKADE, AIRLIFT, NEGOCIATE;
+		BOMB, BLOCKADE, AIRLIFT, DIPLOMACY;
 	}
 	
 	private d_cardType d_type;
 
+	public Card()
+	{
+		issueRandomCard();
+	}
+	
 	public void issueRandomCard()
 	{
 		this.d_type = d_cardType.values()[new Random().nextInt(d_cardType.values().length)];		
@@ -23,9 +30,16 @@ public class Card
 	
 	public static void main(String[] args)
 	{
-		Card c = new Card();
-		c.issueRandomCard();
-		System.out.println(c.getCard());
+		List<Card> c = new ArrayList<>();
+		Card c1;
+		for(int i =0; i<=10; i++)
+		{
+			c1 = new Card();
+			c.add(c1);
+		}
 		
+		
+		for(Card o : c)
+			System.out.println(o.getCard());
 	}
 }
