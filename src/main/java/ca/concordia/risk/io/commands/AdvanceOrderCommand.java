7f2743dcc52dcd5b6c534fce5cdbeb14b949ac35
Sinduck <sinduck7@gmail.com>
2021-03-22 16:45:19 +0530

    Implement AdvanceOrderCommand()

--- src/main/java/ca/concordia/risk/io/commands/AdvanceOrderCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/AdvanceOrderCommand.java
@@ -1,5 +1,15 @@
 package ca.concordia.risk.io.commands;
 
+import ca.concordia.risk.GameEngine;
+import ca.concordia.risk.game.Card;
+import ca.concordia.risk.game.Country;
+import ca.concordia.risk.game.Player;
+import ca.concordia.risk.game.orders.AdvanceOrder;
+import ca.concordia.risk.game.orders.AirliftOrder;
+import ca.concordia.risk.game.orders.DeployOrder;
+import ca.concordia.risk.game.orders.Order;
+import ca.concordia.risk.io.views.ConsoleView;
+
 /**
  * This Class represents an Advance Order Command
  * 
@@ -7,6 +17,32 @@ package ca.concordia.risk.io.commands;
  *
  */
 
-public class AdvanceOrderCommand {
+public class AdvanceOrderCommand implements OrderCommand {
+	private String d_sourceCountry;
+	private String d_targetCountry;
+	private String d_targetPlayer;
+	private int d_numberOfArmies;
+	
+	public AdvanceOrderCommand(String d_sourceCountry, String d_targetCountry, String d_targetPlayer,
+			int d_numberOfArmies) {
+		super();
+		this.d_sourceCountry = d_sourceCountry;
+		this.d_targetCountry = d_targetCountry;
+		this.d_targetPlayer = d_targetPlayer;
+		this.d_numberOfArmies = d_numberOfArmies;
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Order buildOrder(Player p_player) {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+	
 
 }
