fbdcaceba37872b456dfa38b86fd67373d6b23b2
Sinduck <sinduck7@gmail.com>
2021-03-22 18:57:16 +0530

    Final changes and documentation updated

--- src/main/java/ca/concordia/risk/io/commands/AdvanceOrderCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/AdvanceOrderCommand.java
@@ -16,13 +16,20 @@ import ca.concordia.risk.io.views.ConsoleView;
  * @author Sindu
  *
  */
-
 public class AdvanceOrderCommand implements OrderCommand {
 	private String d_sourceCountry;
 	private String d_targetCountry;
 	private String d_targetPlayer;
 	private int d_numberOfArmies;
-	
+
+	/**
+	 * Creates a new <code>AdvanceOrderCommand</code>.
+	 * 
+	 * @param p_sourceCountry  source country which armies are moved from.
+	 * @param p_targetCountry  target country where armies are moved to.
+	 * @param d_targetPlayer   target player whose territory armies are moved to.
+	 * @param p_numberOfArmies number of armies to deploy.
+	 */
 	public AdvanceOrderCommand(String d_sourceCountry, String d_targetCountry, String d_targetPlayer,
 			int d_numberOfArmies) {
 		super();
@@ -31,7 +38,8 @@ public class AdvanceOrderCommand implements OrderCommand {
 		this.d_targetPlayer = d_targetPlayer;
 		this.d_numberOfArmies = d_numberOfArmies;
 	}
-	
+
+	@Override
 	public Order buildOrder(Player p_player) {
 		ConsoleView l_view = GameEngine.GetView();
 
@@ -41,7 +49,7 @@ public class AdvanceOrderCommand implements OrderCommand {
 			l_view.display("Invalid order: source country " + d_sourceCountry + " does not exist");
 			return null;
 		}
-		
+
 		// Validate if player owns the source country
 		if (!p_player.ownsCountry(l_sourceCountry)) {
 			l_view.display("Invalid order: current player does not own the source country " + d_sourceCountry);
@@ -50,29 +58,32 @@ public class AdvanceOrderCommand implements OrderCommand {
 
 		// Validate if the target country exists
 		Country l_targetCountry = GameEngine.GetMap().getCountry(d_targetCountry);
-			if (l_targetCountry == null) {
-				l_view.display("Invalid order: target country " + d_targetCountry + " does not exist");
-				return null;
-			}
-			Player t_player = GameEngine.GetPlayer(d_targetPlayer);
-			if (t_player == null) {
-				l_view.display("Invalid order: target player " + d_targetPlayer + " does not exist");
-				return null;
-			}
-			if (!t_player.ownsCountry(l_targetCountry)) {
-				l_view.display("Invalid order: target player does not own the target country " + d_targetCountry);
-				return null;
-			}
-
-			if (!p_player.retrieveReinforcements(d_numberOfArmies)) {
-				l_view.display("Invalid order: can't move " + d_numberOfArmies + " armies. Only "
-						+ p_player.getRemainingReinforcements() + " reinforcements left");
-				return null;
-			}
-
-			Order l_order = new AdvanceOrder(d_numberOfArmies, p_player, t_player, l_sourceCountry, l_targetCountry);
-			return l_order;
+		if (l_targetCountry == null) {
+			l_view.display("Invalid order: target country " + d_targetCountry + " does not exist");
+			return null;
 		}
+		Player t_player = GameEngine.GetPlayer(d_targetPlayer);
+		if (t_player == null) {
+			l_view.display("Invalid order: target player " + d_targetPlayer + " does not exist");
+			return null;
+		}
+		// Validate if player owns the target country
+		if (!t_player.ownsCountry(l_targetCountry)) {
+			l_view.display("Invalid order: target player does not own the target country " + d_targetCountry);
+			return null;
+		}
+
+		// Validate if player has enough reinforcements. If so, retrieve reinforcements
+		if (!p_player.retrieveReinforcements(d_numberOfArmies)) {
+			l_view.display("Invalid order: can't move " + d_numberOfArmies + " armies. Only "
+					+ p_player.getRemainingReinforcements() + " reinforcements left");
+			return null;
+		}
+
+		// Build and return the order
+		Order l_order = new AdvanceOrder(d_numberOfArmies, p_player, t_player, l_sourceCountry, l_targetCountry);
+		return l_order;
+	}
 
 	@Override
 	public void execute() {
