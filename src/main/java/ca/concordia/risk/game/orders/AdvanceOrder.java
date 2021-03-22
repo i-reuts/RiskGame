1d1a81fb16c2f27f68d4bc54fd45ba93469ad40e
Sinduck <sinduck7@gmail.com>
2021-03-22 14:13:38 +0530

    Create Advance Order Class

---
+++ src/main/java/ca/concordia/risk/game/orders/AdvanceOrder.java
@@ -0,0 +1,26 @@
+package ca.concordia.risk.game.orders;
+
+import ca.concordia.risk.game.Country;
+import ca.concordia.risk.game.Player;
+
+/**
+ * Class to represent AdvanceOrder command
+ * @author Sindu
+ *
+ */
+
+public class AdvanceOrder implements Order{
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
