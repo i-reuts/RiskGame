d627ed70c10f559cc8df0abb6a21ced2fbcf53a6
Sinduck <sinduck7@gmail.com>
2021-03-22 15:12:01 +0530

    Create test for Advance Order

---
+++ src/test/java/ca/concordia/risk/game/orders/AdvanceOrderTest.java
@@ -0,0 +1,8 @@
+package ca.concordia.risk.game.orders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AdvanceOrderTest {
+
+}
