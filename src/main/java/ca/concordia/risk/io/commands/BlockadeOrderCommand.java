da100e3fd54abeacd3a4c5839135d504cf369acb
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2021-03-23 22:48:16 +0530

    implement buildOrder method for blockade order commmand

--- src/main/java/ca/concordia/risk/io/commands/BlockadeOrderCommand.java
+++ src/main/java/ca/concordia/risk/io/commands/BlockadeOrderCommand.java
@@ -1,7 +1,12 @@
 package ca.concordia.risk.io.commands;
 
+import ca.concordia.risk.GameEngine;
+import ca.concordia.risk.game.Card;
+import ca.concordia.risk.game.Country;
 import ca.concordia.risk.game.Player;
+import ca.concordia.risk.game.orders.BlockadeOrder;
 import ca.concordia.risk.game.orders.Order;
+import ca.concordia.risk.io.views.ConsoleView;
 
 public class BlockadeOrderCommand implements OrderCommand {
 	
@@ -9,23 +14,42 @@ public class BlockadeOrderCommand implements OrderCommand {
 	
 	public BlockadeOrderCommand(String p_blockadeCountry) {
 		
+		d_blockadeCountry = p_blockadeCountry;
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
 	public void execute() {
-		
+	
+		ConsoleView l_view = GameEngine.GetView();
+		l_view.display("\nBlockade command to blockade " + d_blockadeCountry + "\n");
 	}
 	
 	
 	public Order buildOrder(Player p_player) {
-		return null;
+		
+		ConsoleView l_view = GameEngine.GetView();
+		
+		// Validate if the player has a blockade card
+		if (!p_player.useCard(Card.getBlockadeCard())) {
+			l_view.display("Invalid order: player " + p_player.getName() + " does not has a bomb card");
+			return null;
+		}
+
+		// Validate if country to be blockade exists
+		Country l_blockadeCountry = GameEngine.GetMap().getCountry(d_blockadeCountry);
+		if (l_blockadeCountry == null) {
+			l_view.display("Invalid order: country " + d_blockadeCountry + " does not exist");
+			return null;
+		}
+		
+		// Validate if player owns the blockade country
+		if (!p_player.ownsCountry(l_blockadeCountry)) {
+				l_view.display("Invalid order: current player does not own " + d_blockadeCountry);
+				return null;
+		}
+		
+		Order l_order = new BlockadeOrder(p_player, l_blockadeCountry);
+		return l_order;
 		
 	}
 
