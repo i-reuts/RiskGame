a2194a9adc37475af7b7677264ecb100115666c5
Illya Reutskyy <iliyareutsky@gmail.com>
2021-03-23 16:43:53 -0400

    Add battle logic to the advance order

--- src/main/java/ca/concordia/risk/game/orders/AdvanceOrder.java
+++ src/main/java/ca/concordia/risk/game/orders/AdvanceOrder.java
@@ -16,7 +16,6 @@ public class AdvanceOrder implements Order {
 	private Country d_sourceCountry;
 	private Country d_targetCountry;
 	private int d_armiesToAdvance;
-	
 
 	/**
 	 * Creates a new <code>AdvanceOrder</code>.
@@ -45,11 +44,15 @@ public class AdvanceOrder implements Order {
 	public void execute() {
 		if (isValid()) {
 			if (!d_player.ownsCountry(d_targetCountry)) {
-				// TODO: Do the battle
+				// Check if the source country has enough armies to attack with.
+				// If not, attack with the amount of armies available.
+				d_status = d_player.getName() + " attacked " + d_targetCountry.getName() + " beloging to "
+						+ d_targetCountry.getOwner().getName() + " from " + d_sourceCountry.getName() + ". ";
 
+				performAttack();
 			} else {
 				// Check if the source country has enough armies to advance.
-				// If not, advance the amount to the number of armies available.
+				// If not, advance the amount of armies available.
 				int l_armiesAvailable = d_sourceCountry.getArmies();
 				if (l_armiesAvailable < d_armiesToAdvance) {
 					d_status = d_player.getName() + " advanced " + l_armiesAvailable + " armies (out of "
@@ -65,7 +68,6 @@ public class AdvanceOrder implements Order {
 				d_sourceCountry.removeArmies(d_armiesToAdvance);
 				d_targetCountry.addArmies(d_armiesToAdvance);
 			}
-
 		}
 	}
 
@@ -105,4 +107,77 @@ public class AdvanceOrder implements Order {
 
 		return true;
 	}
+
+	/**
+	 * Executes battle between the attacking and the defending armies.
+	 * <p>
+	 * If there are attacker armies remaining and no defender armies remaining, the
+	 * target country is conquered.<br>
+	 * Otherwise remaining armies are returned to their respective countries.
+	 */
+	private void performAttack() {
+		// Remove armies from their countries
+		int l_defenderArmies = d_targetCountry.getArmies();
+		int l_availableAttackerArmies = d_sourceCountry.getArmies();
+		int l_attackerArmies;
+		if (d_armiesToAdvance > l_availableAttackerArmies) {
+			l_attackerArmies = l_availableAttackerArmies;
+			d_status += "Attacker armies: " + l_attackerArmies + " (out of " + d_armiesToAdvance
+					+ " requested), defender armies: " + l_defenderArmies + ". ";
+		} else {
+			l_attackerArmies = d_armiesToAdvance;
+			d_status += "Attacker armies: " + l_attackerArmies + ", defender armies: " + l_defenderArmies + ". ";
+		}
+
+		d_sourceCountry.removeArmies(l_attackerArmies);
+		d_targetCountry.removeArmies(l_defenderArmies);
+
+		// Calculate the number of defenders defeated
+		int l_defendersDefeated = 0;
+		for (int i = 0; i < l_attackerArmies; i++) {
+			if (Math.random() <= 0.6) {
+				l_defendersDefeated++;
+			}
+		}
+
+		// Calculate the number of attackers defeated
+		int l_attackersDefeated = 0;
+		for (int i = 0; i < l_defenderArmies; i++) {
+			if (Math.random() <= 0.7) {
+				l_attackersDefeated++;
+			}
+		}
+
+		// Decrease the number of attackers and defenders
+		l_attackerArmies -= l_attackersDefeated;
+		if (l_attackerArmies < 0) {
+			l_attackerArmies = 0;
+		}
+
+		l_defenderArmies -= l_defendersDefeated;
+		if (l_defenderArmies < 0) {
+			l_defenderArmies = 0;
+		}
+
+		// If remaining attacker armies are greater than 0 and defender armies is 0,
+		// capture the territory
+		if (l_attackerArmies > 0 && l_defenderArmies == 0) {
+			d_targetCountry.getOwner().removeCountry(d_targetCountry);
+
+			d_targetCountry.setOwner(d_player);
+			d_player.addCountry(d_targetCountry);
+
+			d_targetCountry.addArmies(l_attackerArmies);
+
+			d_status += "Country conquered succesfully " + " with " + l_attackerArmies + " armies remaining.";
+		} else {
+			// Otherwise return remaining armies back to their respective countries
+			d_sourceCountry.addArmies(l_attackerArmies);
+			d_targetCountry.addArmies(l_defenderArmies);
+
+			d_status += "Country was not conquered. " + l_attackerArmies + " attacker armies and " + l_defenderArmies
+					+ " defender armies remained.";
+		}
+	}
+
 }
