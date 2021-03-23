d9a044ecb54da781a7f0ab13c6e1890fe53ed695
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2021-03-23 21:37:33 +0530

    inital implementation for Blockade order command

---
+++ src/main/java/ca/concordia/risk/io/commands/BlockadeOrderCommand.java
@@ -0,0 +1,32 @@
+package ca.concordia.risk.io.commands;
+
+import ca.concordia.risk.game.Player;
+import ca.concordia.risk.game.orders.Order;
+
+public class BlockadeOrderCommand implements OrderCommand {
+	
+	private String d_blockadeCountry;
+	
+	public BlockadeOrderCommand(String p_blockadeCountry) {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public void execute() {
+		
+	}
+	
+	
+	public Order buildOrder(Player p_player) {
+		return null;
+		
+	}
+
+}
