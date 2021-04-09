24593b08d6e6728348f474e99ae1b35ecba7392b
enrique <menriquemcm@gmail.com>
2021-04-09 15:07:30 -0500

    Improve javadoc for aggressive strategy

--- src/main/java/ca/concordia/risk/game/strategies/AggressiveStrategy.java
+++ src/main/java/ca/concordia/risk/game/strategies/AggressiveStrategy.java
@@ -1,7 +1,6 @@
 package ca.concordia.risk.game.strategies;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -13,8 +12,12 @@ import ca.concordia.risk.game.orders.AdvanceOrder;
 import ca.concordia.risk.game.orders.DeployOrder;
 import ca.concordia.risk.game.orders.Order;
 
-
-
+/**
+ * This class represents the aggressive player strategy.
+ * <p>
+ * A <code>Player</code> that concentrates all his armies into one country and
+ * attack!
+ */
 public class AggressiveStrategy extends PlayerStrategy {
 	// Key = node , Value = backtrack node
 	HashMap<Country, Country> d_backtrack = new HashMap<Country, Country>();
@@ -23,11 +26,21 @@ public class AggressiveStrategy extends PlayerStrategy {
 	Country d_countryToDeploy = null;
 	int d_amountToDeploy = 0;
 	boolean d_hasAdvance = false;
-	
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public AggressiveStrategy(Player p_player) {
 		super(p_player);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * Aggressive behavior for issuing orders. - Find the country with your armies
+	 * and deploy all armies in it. - Find the nearest enemy with a BFS approach. -
+	 * Either attack an enemy or start advancing towards one.
+	 */
 	@Override
 	public Order issueOrder() {
 		d_countryList = new ArrayList<Country>(d_player.getCountries());
@@ -36,7 +49,7 @@ public class AggressiveStrategy extends PlayerStrategy {
 			if (d_amountToDeploy > 0) {
 				// We will deploy all armies
 				d_player.retrieveReinforcements(d_amountToDeploy);
-				
+
 				// Look up for a country that already has armies
 				for (Country l_c : d_countryList) {
 					if (l_c.getArmies() > 0) {
@@ -44,7 +57,7 @@ public class AggressiveStrategy extends PlayerStrategy {
 						return new DeployOrder(d_player, d_countryToDeploy, d_amountToDeploy);
 					}
 				}
-				
+
 				// If no country has armies, look up for a country with enemies
 				if (d_countryToDeploy == null) {
 					for (Country l_c : d_countryList) {
@@ -58,18 +71,19 @@ public class AggressiveStrategy extends PlayerStrategy {
 				}
 			}
 		}
-		
+
+		// Advance once per turn
 		if (!d_hasAdvance) {
 			Country l_enemy;
 			l_enemy = findEnemy(d_countryToDeploy);
 			buildPath(l_enemy);
 			if (!d_path.isEmpty()) {
 				d_hasAdvance = true;
-				System.out.println("Advance " + (d_countryToDeploy.getArmies() + d_amountToDeploy));
-				return new AdvanceOrder(d_player, d_countryToDeploy, d_path.pop(), d_countryToDeploy.getArmies() + d_amountToDeploy);
+				return new AdvanceOrder(d_player, d_countryToDeploy, d_path.pop(),
+						d_countryToDeploy.getArmies() + d_amountToDeploy);
 			}
 		}
-		
+
 		// Finish issuing orders
 		d_hasAdvance = false;
 		d_countryToDeploy = null;
@@ -77,23 +91,25 @@ public class AggressiveStrategy extends PlayerStrategy {
 		d_player.setFinishedIssuingOrder(true);
 		return null;
 	}
-	
-	public Country findEnemy (Country p_fromCountry) {
-		// Key = Country, Value = Backtrack node
 
-		LinkedList< Country> l_queue = new LinkedList<Country>();
+	/**
+	 * Populate d_backtrack with a BFS approach, until finding an enemy
+	 * 
+	 * @param p_fromCountry Current country to trace a path to an enemy country.
+	 * @return Enemy Country to advance towards
+	 */
+	public Country findEnemy(Country p_fromCountry) {
+		LinkedList<Country> l_queue = new LinkedList<Country>();
 		HashSet<Country> l_visited = new HashSet<Country>();
 		Country l_tmp;
-		
-		// clean the stack if peek returns an enemy
-		
+
 		l_queue.add(p_fromCountry);
 		l_visited.add(p_fromCountry);
 		d_backtrack.put(p_fromCountry, null);
-		
+
 		while (l_queue.size() > 0) {
 			l_tmp = l_queue.poll();
-			
+
 			for (Country l_c : l_tmp.getNeighbors()) {
 				if (l_c.getOwner().getName().equals(d_player.getName())) {
 					if (!l_visited.contains(l_c)) {
@@ -103,14 +119,20 @@ public class AggressiveStrategy extends PlayerStrategy {
 					}
 					continue;
 				}
-				d_backtrack.put(l_c, l_tmp); 
+				d_backtrack.put(l_c, l_tmp);
 				return l_c;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
+	/**
+	 * Populate d_path walking back in d_backtrack from the target enemy until the
+	 * current source country.
+	 * 
+	 * @param p_enemy Enemy Country to trace a path to.
+	 */
 	public void buildPath(Country p_enemy) {
 		Country l_tmp = p_enemy;
 		while (d_backtrack.get(l_tmp) != null) {
