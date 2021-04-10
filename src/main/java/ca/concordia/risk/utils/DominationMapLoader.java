af3f31617894dad931abf7b9ea5f59e30eaebf24
Sinduck <sinduck7@gmail.com>
2021-04-10 05:43:59 +0530

    Create Domination Map Loader

---
+++ src/main/java/ca/concordia/risk/utils/DominationMapLoader.java
@@ -0,0 +1,13 @@
+package ca.concordia.risk.utils;
+
+/**
+ * This class provides functionality to load the map from and save the map to
+ * <i>.map</i> files.
+ * 
+ * @author Sindu
+ */
+
+public class DominationMapLoader {
+
+	
+}
