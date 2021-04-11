0238e859660205083c222e900583d2d78d368523
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2021-04-11 22:54:57 +0530

    add constructor for Tournament class

--- src/main/java/ca/concordia/risk/game/Tournament.java
+++ src/main/java/ca/concordia/risk/game/Tournament.java
@@ -6,10 +6,20 @@ import java.util.Map;
 public class Tournament {
 	
 	private int d_gameId;
-	private int d_numTurns;
-	private GameMap d_map;
-	private Map<String, Player> d_playerTypes;
+	private int d_turns;
+	private ArrayList<String> d_mapFiles;
+	private ArrayList<String> d_playerStrategies;
 	private ArrayList<String> d_tournamentResult;
 	
-
+	
+	public Tournament(ArrayList<String> p_mapFiles, ArrayList<String> p_playerStrategies, int p_gameId, int p_turns) {
+		d_mapFiles = p_mapFiles;
+		d_playerStrategies = p_playerStrategies;
+		d_gameId = p_gameId;
+		d_turns = p_turns;
+		d_tournamentResult = new ArrayList<String>;  
+		
+	}
+	
+	
 }
