00eb0eb4e510d8e139957c08677fb29e4b84f026
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2021-04-11 20:35:59 +0530

    update attributes for Tournament

--- src/main/java/ca/concordia/risk/game/Tournament.java
+++ src/main/java/ca/concordia/risk/game/Tournament.java
@@ -1,5 +1,15 @@
 package ca.concordia.risk.game;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Tournament {
+	
+	private int d_gameId;
+	private int d_numTurns;
+	private GameMap d_map;
+	private Map<String, Player> d_playerTypes;
+	private ArrayList<String> d_tournamentResult;
+	
 
 }
