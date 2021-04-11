abc3e8794d3bbe349e65ae4c936206d732de56ba
ShubhamVashisth7 <shubham.vashisth.delhi@gmail.com>
2021-04-11 23:16:43 +0530

    add getters and setters for Tournament class

--- src/main/java/ca/concordia/risk/game/Tournament.java
+++ src/main/java/ca/concordia/risk/game/Tournament.java
@@ -17,9 +17,57 @@ public class Tournament {
 		d_playerStrategies = p_playerStrategies;
 		d_gameId = p_gameId;
 		d_turns = p_turns;
-		d_tournamentResult = new ArrayList<String>;  
-		
+		d_tournamentResult = new ArrayList<String>();  
 	}
+
+	public ArrayList<String> getTournamentResult() {
+		return d_tournamentResult;
+	}
+	
+	public ArrayList<String> getMapFiles() {
+		return d_mapFiles;
+	}
+	
+	public void setMapFiles(ArrayList<String> p_mapFiles) {
+		d_mapFiles = p_mapFiles;
+	}
+
+	public ArrayList<String> getPlayerStrategies() {
+		return d_playerStrategies;
+	}
+
+	public void setPlayerStrategies(ArrayList<String> p_playerStrategies) {
+		d_playerStrategies = p_playerStrategies;
+	}
+	
+	public int getGameId() {
+		return d_gameId;
+	}
+
+	public void setGameId(int p_gameId) {
+		d_gameId = p_gameId;
+	}
+
+	public int getTurns() {
+		return d_turns;
+	}
+
+	public void setTurns(int p_turns) {
+		d_turns = p_turns;
+	}
+
+
+
+
+
+
+	/**
+	 * @param d_tournamentResult the d_tournamentResult to set
+	 */
+	public void setD_tournamentResult(ArrayList<String> d_tournamentResult) {
+		this.d_tournamentResult = d_tournamentResult;
+	}
+	
 	
 	
 }
