8cbc35d66d0f39e7d7d83fd7b37eb492ac289e31
Illya Reutskyy <iliyareutsky@gmail.com>
2021-04-13 16:31:39 -0400

    Add issueOrder test

--- src/test/java/ca/concordia/risk/game/PlayerTest.java
+++ src/test/java/ca/concordia/risk/game/PlayerTest.java
@@ -4,9 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import ca.concordia.risk.GameEngine;
+import ca.concordia.risk.game.orders.DeployOrder;
+import ca.concordia.risk.game.orders.Order;
+import ca.concordia.risk.game.strategies.HumanStrategy;
 
 /**
  * Unit test class for the <code>Player</code>.
@@ -106,5 +117,59 @@ class PlayerTest {
 		assertTrue(l_player.useCard(Card.getAirliftCard()));
 		assertTrue(l_player.useCard(Card.getAirliftCard()));
 		assertFalse(l_player.useCard(Card.getAirliftCard()));
+	}
+
+	/**
+	 * Tests order issuing by the human player.
+	 * 
+	 * @throws IOException thrown if the stream pipe setup fails.
+	 */
+	@Test
+	void testIssuingOrders() throws IOException {
+		// Set up a mock input stream and redirect output stream
+		InputStream l_defaultInputStream = System.in;
+		PipedInputStream l_mockInputStream = new PipedInputStream();
+		PrintWriter l_mockInputStreamWriter = new PrintWriter(new PipedOutputStream(l_mockInputStream), true);
+		System.setIn(l_mockInputStream);
+		
+		PrintStream l_defaultOutputStream = System.out;
+		System.setOut(new PrintStream(PrintStream.nullOutputStream()));
+
+		// Create a simple map and a player
+		GameMap l_map = new GameMap();
+		Continent l_continent = new Continent("Test Continent", 3);
+		Country l_country = new Country("Test Country", l_continent);
+		l_map.addContinent(l_continent);
+		l_map.addCountry(l_country);
+
+		Player l_player = new Player("Test Player");
+		l_player.SetStrategy(new HumanStrategy(l_player));
+		l_player.addCountry(l_country);
+		l_country.setOwner(l_player);
+		l_player.assignReinfocements();
+		
+		// Initialize the game engine and switch to the gameplay phase
+		GameEngine.Initialize();
+		GameEngine.SetMap(l_map);
+		GameEngine.SwitchToNextPhase();
+		GameEngine.SwitchToNextPhase();
+
+		// Write a deploy order to the input stream
+		l_mockInputStreamWriter.println("deploy Test_Country 3");
+
+		// Ask the player to issue order
+		l_player.issueOrder();
+
+		// Peak issued order, ensure it is a correct deploy order
+		Order l_order = l_player.peekLastOrder();
+		assertTrue(l_order instanceof DeployOrder);
+
+		l_order.execute();
+		assertEquals(l_order.getStatus(), "Test Player deployed 3 armies to Test Country");
+
+		// Restore the default input stream
+		l_mockInputStreamWriter.close();
+		System.setIn(l_defaultInputStream);
+		System.setOut(l_defaultOutputStream);
 	}
 }
